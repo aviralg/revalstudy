2d71b7c1ef0557d1d72d8607048ea19c716bbc62
Filip Krikava <krikava@gmail.com>
2020-08-11 17:39:21 +0000

    Add corpus-analysis

---
+++ inst/analysis/inc/paths.R
@@ -0,0 +1,6 @@
+## corpus only
+CORPUS_FILE                  <- path(params$output_dir, "corpus.txt")
+CORPUS_REVDEPS               <- path(params$output_dir, "corpus-revdeps.csv")
+CORPUS_REVDEPS_FILE          <- path(params$output_dir, "corpus-revdeps.txt")
+
+TAGS_DIR <- params$output_dir
---
+++ inst/analysis/inc/setup.R
@@ -0,0 +1,7 @@
+is_outlier_min <- function(x, m=1.5) quantile(x, 0.25) - m * IQR(x)
+
+is_outlier_max <- function(x, m=1.5) quantile(x, 0.75) + m * IQR(x)
+
+is_outlier <- function(x, m=1.5) {
+  (x < is_outlier_min(x, m)) | (x > is_outlier_max(x, m))
+}
